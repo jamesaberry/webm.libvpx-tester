4a2146d76362a6b174d534ee337d34c359fb01db
James Berry <jamesberry@google.com>
2011-05-24 16:26:37 -0400

    semi colon missing
    Change-Id: Ibe687aaf622624e358aed54bf80e762c91dc1902

--- MasterFile/src/util/utilities.cpp
+++ MasterFile/src/util/utilities.cpp
@@ -5005,7 +5005,7 @@ int  vpxt_eval_new_vs_old_log(const char *logfile, std::string TestIDStr, int pr
             logFile.getline(logFileLine, 256);
     }
 
-    logFile.close()
+    logFile.close();
     return 0;
 }
 int  vpxt_check_arg_input(const char *testName, int argNum)
